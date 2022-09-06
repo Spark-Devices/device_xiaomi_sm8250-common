f1f4f66145b0e52e7c98da4031a039cff6a66296
spkal01 <kalligeross@gmail.com>
2022-09-06 17:27:07 +0300

    sm8250-common: Init spark

--- kona.mk
+++ kona.mk
@@ -348,7 +348,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-spark
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
