197280996c2ecf48a06a376dcf796beb592725cb
johnmart19 <johnivan19999@gmail.com>
2022-11-05 07:49:10 +0200

    sm8250-common: Add support of Miui Dolby and MiSound
    Usage:
    
    If wanted to use with Dolby:
    Add TARGET_USES_MIUI_DOLBY := true to device tree, Import Dolby engine and Dolby codecs
    
    Otherwise:
    Will be available MiSound (Dirac) as default audio engine
    
    - Dolby Config Decommonized to device specific DT
    - Sepolicy is reworked and moved to sepolicy/dolby with enabling to build only when TARGET_USES_MIUI_DOLBY is true
    - Added creation of data/vendor/dolby and multimedia folders on early boot
    - Imported missing soundfx modules and import AudioSphere deps
    - Amount of props is exactly minimal for proper work of requested stuffs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -234,7 +234,12 @@ ifeq ($(TARGET_IS_VAB),true)
 BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 endif
 
-# WiFi
+ifeq ($(TARGET_USES_MIUI_DOLBY),true)
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+     $(COMMON_PATH)/dolby/manifests/dolby_framework_matrix.xml
+endif
+
+# Wi-Fi
 BOARD_WLAN_DEVICE := qcwcn
 BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
