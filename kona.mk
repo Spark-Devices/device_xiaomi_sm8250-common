1fbba76a8b874049afdee16cd5e3ec74883902b2
Arian <arian.kulmer@web.de>
2023-09-02 12:29:08 +0200

    sm8250-common: Switch to xiaomi vibrator aidl
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- kona.mk
+++ kona.mk
@@ -532,10 +532,10 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi_kona
+    vendor.qti.hardware.vibrator.service.xiaomi
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    hardware/xiaomi/aidl/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # WiFi
 PRODUCT_PACKAGES += \
